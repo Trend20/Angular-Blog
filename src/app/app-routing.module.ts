4f3f015306bd4c5e08b63636cc2288c8fd62a169
Trend20 <enockomondi305@gmail.com>
2022-10-26 08:12:59 +0300

    additional routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { AddArticleComponent } from './articles/add-article/add-article.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MyArticlesComponent } from './articles/my-articles/my-articles.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { UserComponent } from './user/user.component';
@@ -9,6 +12,9 @@ const routes: Routes = [
   {path: 'user', component: UserComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'my-articles', component: MyArticlesComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'add-article', component: AddArticleComponent},
   {path: '', redirectTo: 'user', pathMatch: 'full'}
 ];
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isLoggedIn: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
